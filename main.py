fa2e65203e3c974c8740b368e15b259d45c14053
Alexandru <alexandru.akkol@gmail.com>
2022-03-02 18:46:08 +0200

    procfile

--- main.py
+++ main.py
@@ -11,8 +11,29 @@ studioSale = {'studioSale': all_data['studioSale']}
 
 @app.route('/')
 def homepage():
-    return 'try /apartments-for-rent, /apartments-for-sale, /studios-for-rent, or /studios-for-sale'
-
+    return ("""
+    <style>
+    *{text-align:center;
+    list-style:none;}
+    h1{
+    font-size:calc(12px + 3vw);
+    }
+    ul{
+    font-size:calc(12px + 2vw);}
+    </style>
+    
+    <h1>Bucharest real estate prices API</h1>
+    <ul>
+        <li><a href="/apartments-for-rent">Apartments for rent API</a></li>
+        <li><a href="/apartments-for-sale">Apartments for sale API</a></li>
+        <li><a href="/studios-for-rent">Studios for rent API</a></li>
+        <li><a href="/studios-for-sale">Studios for sale API</a></li>
+    </ul>
+    <p style="display:block;height:70vh;">Data format: €/m²</p>
+    <footer>
+    Data from compariimobiliare.ro
+    </footer>
+    """)
 @app.route('/apartments-for-rent')
 def apartmentRentPage():
     return apartmentRent
